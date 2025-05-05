4ffb3dff977a12d076f130840eec50cc5155a64f
mujlax <42385663+mujlax@users.noreply.github.com>
2025-05-05 16:09:40 +0300

    Update Ticker_Control.user.js

--- Ticker_Control.user.js
+++ Ticker_Control.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Hotkey Ticker Control
+// @name         Ticker Control
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Меняй framerate по хоткею
 // @match        *://*/*
 // @grant        none
